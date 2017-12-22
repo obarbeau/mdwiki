ef9f232a7eb5d236b1037ebdddf7920bee19c6d3
Olivier Barbeau <obarbeau@acs-ami.com>
2017-12-22 11:02:38 +0100

    more css

--- js/ts/wiki.ts
+++ js/ts/wiki.ts
@@ -91,11 +91,15 @@ module MDwiki.Core {
             marked.setOptions(options);
 
             // get sample markdown
-            markdown=markdown.replace(/\[(OK)\](.*)/gi, "<div class=\"$1 fa fa-check-square-o\">&nbsp;$2");
-            markdown=markdown.replace(/\[(hgh)\](.*)/gi, "<div class=\"$1 fa fa-arrow-up\">&nbsp;$2");
-            markdown=markdown.replace(/\[(low)\](.*)/gi, "<div class=\"$1 fa fa-arrow-down\">&nbsp;$2");
-            markdown=markdown.replace(/\[(cxl)\](.*)/gi, "<div class=\"$1 fa fa-ban\">&nbsp;$2");
-            markdown=markdown.replace(/\[(sby)\](.*)/gi, "<div class=\"$1 fa fa-spinner\">&nbsp;$2");
+markdown=markdown.replace(/\[(OK)\](.*)/gi, "<div class=\"$1 fa fa-check-square-o\">&nbsp;$2");
+markdown=markdown.replace(/\[(NOK)\](.*)/gi, "<div class=\"$1 fa-thumbs-down\">&nbsp;$2");
+markdown=markdown.replace(/\[(now)\](.*)/gi, "<div class=\"$1 fa-arrow-circle-right\">&nbsp;$2");
+markdown=markdown.replace(/\[(wrn)\](.*)/gi, "<div class=\"$1 fa-warning\">&nbsp;$2");
+markdown=markdown.replace(/\[(bug)\](.*)/gi, "<div class=\"$1 fa fa-bomb\">&nbsp;$2");
+markdown=markdown.replace(/\[(hgh)\](.*)/gi, "<div class=\"$1 fa fa-arrow-up\">&nbsp;$2");
+markdown=markdown.replace(/\[(low)\](.*)/gi, "<div class=\"$1 fa fa-arrow-down\">&nbsp;$2");
+markdown=markdown.replace(/\[(cxl)\](.*)/gi, "<div class=\"$1 fa fa-ban\">&nbsp;$2");
+markdown=markdown.replace(/\[(sby)\](.*)/gi, "<div class=\"$1 fa fa-spinner\">&nbsp;$2");
 
             var transformer = new MDwiki.Markdown.Markdown(markdown, options);
             var html = transformer.transform();
